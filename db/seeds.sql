92ab56e9f31711b26d94ced84b541dc5351c19d5
JJ Cardenas <jjcardenas16@hotmail.com>
2020-04-16 22:59:13 -0700

    functional app

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Sweet Home Avocado');
+INSERT INTO burgers (burger_name) VALUES ('Gourdon Hamsey');
